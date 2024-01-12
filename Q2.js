72e5ddc9457a2f6f6d519ffe2a9b5f3ab53a4a7c
Priya Agrawal <darshankshetri8@gmail.com>
2024-01-12 16:14:56 +0530

    Add new js code

---
+++ Q2.js
@@ -0,0 +1,13 @@
+/*Q2) Create a const object called "Profile" to store more information show in the picture */
+
+const profile = {
+    profile: "Instagram-Acoount",
+    Insta_Id: "Darshan Kshetri",
+    posts: "25",
+    followers: "56k",
+    following: "58",
+    Bio: "Student of Scientific Computing | Software Devloper",
+};
+
+console.log(profile);
+
