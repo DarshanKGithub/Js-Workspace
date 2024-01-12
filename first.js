3a6e690d7404e052c4e54b0d7540adda3348209e
Priya Agrawal <darshankshetri8@gmail.com>
2024-01-12 15:48:33 +0530

    Add new Paragraph

---
+++ first.js
@@ -0,0 +1,35 @@
+/*console.log("Welcome to Dk's Js-Workspace");
+console.log("Darshan Kshetri");
+console.log("I Love to to programmming");
+console.log("Congratulations writing a first js code")
+
+name="Darshan KShetri";
+console.log(name); 
+age = 24;
+console.log(age);
+price = 66.33;
+console.log(price);
+
+x = null;
+console.log(x)
+
+y = undefined;
+console.log(y)
+
+isFollow=false;
+console.log(isFollow);
+
+let name = "DarshanKGithub";
+console.log(name);
+*/
+
+const student = {
+    fullName: "Darshan",
+    age: 22,
+    cgpa: 9.6,
+    isPass:true,
+};
+
+student["age"] = student["age"] + 9;
+
+console.log(student["age"]) ;
