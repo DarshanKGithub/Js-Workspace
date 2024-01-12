b3450a773e9d1bcc90e02d740f416889b4f5cd88
Priya Agrawal <darshankshetri8@gmail.com>
2024-01-12 16:41:29 +0530

    Add new Code

---
+++ arithmatic.js
@@ -0,0 +1,28 @@
+// //Arithmatic operator 
+
+// let a = 58;
+// let b = 98;
+
+//   console.log("a =",a , " b = ", b);//Showing the value of variable
+//   console.log("a + b =", a + b);//Addition
+//   console.log("a - b =", a - b);//Substraction
+//   console.log("a * b =", a * b);//Multiplication
+//   console.log("a / b =", a / b);//Division
+//   console.log("a % b =", a % b);//Modulas
+//   console.log("a ** b =", a ** b)//58^98 Exponential, Power of numbers
+  
+
+
+//Unary Opertors
+
+  let a = 58;
+ let b = 98;
+
+  console.log("a =",a , " b = ", b);//Showing the value of variable
+  // a = a + 5;
+  //a++;
+  //a--;
+  // ++a;
+  --a;
+
+  console.log(a);
