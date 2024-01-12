72b1dcee5d85ad2293e7bdda9cfd301e990f32da
Priya Agrawal <darshankshetri8@gmail.com>
2024-01-12 16:03:02 +0530

    Add new code

---
+++ Q1.js
@@ -0,0 +1,12 @@
+/*Q1)Create a const object called "product" to store information show in the pitcure.
+Ref="Apna Collge" */
+
+const product = {
+    productName: "Parker Jotter Standard CT Ball Pen",
+    Deal_of_the_day: true,
+    price: 270,
+    off: "5%",
+
+}
+
+console.log(product);
