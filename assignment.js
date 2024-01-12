79dfeb19a28423bc3a8ddd259f3cbd18a8b4b8d8
Priya Agrawal <darshankshetri8@gmail.com>
2024-01-12 16:54:52 +0530

    Assignment operator

---
+++ assignment.js
@@ -0,0 +1,14 @@
+//Assignment operators
+
+let a = 9;
+let b = 8;
+
+//  a += 4; //a = a + 4;
+//  a -= 4; //a = a - 4;
+//  a *= 4; //a = a * 4;
+//  a /= 4; //a = a / 4;
+//  a %= 4; //a = a % 4;
+ a **= 4; //a = a ** 4;
+console.log(a);
+
+
