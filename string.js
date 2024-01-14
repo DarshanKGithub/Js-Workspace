8d955dd79c08eaf820bae78b650e280230e54f4d
Darshan Kshetri <126147538+DarshanKGithub@users.noreply.github.com>
2024-01-14 16:33:54 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,26 @@
+
+// // let str = "Darshan Kshetri";
+// // console.log(str);
+
+// // let str = "Darshan Kshetri"
+// // console.log(str.length);
+
+// let str = "Darshan Kshetri"
+// console.log(str[0],str[8]);
+
+// //Templete literals - Special type of string
+
+// let specialString=("I'm Lazy enough");
+
+// console.log(typeof specialString);// typeof for determining the object whether that is string,object.integers etc
+
+
+
+let obj = {
+    item: "pen",
+    price: "25",
+}
+
+let output = `the cost of ${obj.item} is ${obj.price}`
+console.log(output);
+// console.log("the cost of", obj.item,"is:",obj.price);
