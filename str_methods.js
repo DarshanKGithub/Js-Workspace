003e01d7c5327a698a9a11069132ce122c7368f6
Darshan Kshetri <126147538+DarshanKGithub@users.noreply.github.com>
2024-01-14 17:05:18 +0530

    Add files via upload

---
+++ str_methods.js
@@ -0,0 +1,24 @@
+//String Method                                                             
+
+// str.toUpperCase()
+
+// let str = "Coding life Sucks!!!"
+// console.log(str.toUpperCase);
+
+// //str.toLowerCase()
+
+// console.log(str.toLowerCase);
+    
+
+// let str = "Dkoder!!! Full stack developer";
+// let newStr = str.toUpperCase();
+
+
+// console.log(str);
+// console.log(newStr);
+
+// let str = "Dkoder!!! Full Stack Developer"
+// let newStr = str.toLowerCase()
+
+// console.log(newStr);
+// console.log(str);
