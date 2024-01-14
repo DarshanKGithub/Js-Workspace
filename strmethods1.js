1a7d30814cde68f7520aba36471b682786515b86
Darshan Kshetri <126147538+DarshanKGithub@users.noreply.github.com>
2024-01-14 17:13:37 +0530

    Add files via upload

---
+++ strmethods1.js
@@ -0,0 +1,9 @@
+// String Methods
+
+1.str.toLowerCase();
+2.str.toUpperCase();
+3.str.trim();
+4.str.slice(start,end?)//return part of slicing
+5.str.concat(str2)//join str2 to str1
+6.str.replace(searchVal,newVal)
+7.str.charAt(idx)
