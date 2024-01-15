b544c1751290075f4841e9c0a5fab881419516a7
Darshan Kshetri <126147538+DarshanKGithub@users.noreply.github.com>
2024-01-15 19:15:14 +0530

    Add files via upload

---
+++ pop.js
@@ -0,0 +1,5 @@
+let foodItems = ["potato", "apple","grapes","mango","banana"];
+console.log(foodItems);
+let deletedItems=foodItems.pop();
+console.log(foodItems);
+console.log("deleted", deletedItems);
