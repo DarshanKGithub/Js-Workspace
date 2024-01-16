6cee3223c9d4a5f48e21899e53bda1f8b03d8ac7
Darshan Kshetri <126147538+DarshanKGithub@users.noreply.github.com>
2024-01-16 19:18:34 +0530

    Add files via upload

---
+++ Q9.js
@@ -0,0 +1,13 @@
+/*Q9) Create an array to store companies -> "Blomberg", "Microsoft","Uber","Google","IBM","Netflif" 
+
+a) Remove the first company from the array
+b)Remove Uber & Add Ola in its place 
+c) Add Amazon at the end*/
+
+let companies = ["Blomberg", "Microsoft","Uber","Google","IBM","Netflif" ];
+
+//companies.splice(2,1,"Ola");
+// console.log(companies)
+
+companies.push("Amazon");
+console.log(companies)
