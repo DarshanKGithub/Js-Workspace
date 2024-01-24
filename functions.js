ca03672b78eef37563caaa15c891e99ef09ee791
Darshan Kshetri <126147538+DarshanKGithub@users.noreply.github.com>
2024-01-24 21:11:21 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,37 @@
+console.log("p")
+console.log("r")
+console.log("i")
+console.log("y")
+console.log("a")
+
+function saymyname(){
+    console.log("p")
+console.log("r")
+console.log("i")
+console.log("y")
+console.log("a")
+}
+saymyname()
+
+function add2numbers(num1,num2){
+    console.log(3+4)
+}
+add2numbers(3,4)
+
+function loggedinusername(username){
+console.log('(priya)just logged in')
+// }myNewArray
+loggedinusername("priya")
+
+// const myNewArray=[200,400,600,800,1000]
+function arrayvalues(myarray){
+    
+     result(myarray[2])
+}
+console.log(arrayvalues(myNewArray));
+
+function multiply(a,b){
+    b=typeof b!=="undefined"?b:1;
+return a*b;
+}
+console.log (multiply(5)); 
